d11acd4b988d99bf667acc1760fb55ea63f3d5b9
raphael <rtnanje@gmail.com>
2019-11-08 07:53:58 -0500

    Init

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='websocket-commands',
+    version='0.0.1',
+    packages=['websocket_commands'],
+    url='http://github.com/en-lofty/websocket-commands.git',
+    license='',
+    author='raphael',
+    author_email='rtnanje@gmail.com',
+    description='A library that makes communicating between frontend and backend websockets simple.',
+    install_requires=['']
+)
